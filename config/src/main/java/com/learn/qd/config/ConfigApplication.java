64deee1c633600ecd536fb0c0a26313d61931a29
zms00@139.com <zms00@139.com>
2019-03-31 09:31:02 +0800

    config

---
+++ config/src/main/java/com/learn/qd/config/ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.learn.qd.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
